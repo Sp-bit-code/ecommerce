ec718a6335c466492217127a8c69f51f53a53689
Sp-bit-code <sparshsrivastava10d@gmail.com>
2025-06-08 14:02:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// import ShopContextProvider from './Context/ShopContext';
+// import { AuthProvider } from "../src/Context/AuthContext";
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <AuthProvider>
+//     <ShopContextProvider>
+//     <App />
+//     </ShopContextProvider>
+//     </AuthProvider>
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ShopContextProvider from './Context/ShopContext';
-import { AuthProvider } from "../src/Context/AuthContext";
+import { AuthProvider } from './Context/AuthContext';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-    <ShopContextProvider>
-    <App />
-    </ShopContextProvider>
-    </AuthProvider>
+    <BrowserRouter basename="/">
+      <AuthProvider>
+        <ShopContextProvider>
+          <App />
+        </ShopContextProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
